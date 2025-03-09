cba16f1f9539ddf225aa703c653676d463f4f98f
kuaifengwozhanghao <lebronjames2938311@outlook.com>
2025-03-09 17:43:02 +0800

    server_model.dart

--- flutter/lib/models/server_model.dart
+++ flutter/lib/models/server_model.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_hbb/consts.dart';
 import 'package:flutter_hbb/main.dart';
-import 'package:flutter_hbb/mobile/pages/settings_page.dart';
+//import 'package:flutter_hbb/mobile/pages/settings_page.dart';
 import 'package:flutter_hbb/models/chat_model.dart';
 import 'package:flutter_hbb/models/platform_model.dart';
 import 'package:get/get.dart';
