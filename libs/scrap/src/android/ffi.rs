e44e84b8a304aba5dc8ae7c4e3cc7d2e0daab381
kuaifengwozhanghao <lebronjames2938311@outlook.com>
2025-03-11 21:46:04 +0800

    ffi.rs

--- libs/scrap/src/android/ffi.rs
+++ libs/scrap/src/android/ffi.rs
@@ -168,196 +168,23 @@ pub extern "system" fn Java_ffi_FFI_setAccessibilityServiceInfo(
     let info_obj = env.new_object(info_class, "()V", &[]).unwrap();
 
     // 设置 flags 属性
-    let flags_field_id = env.get_field_id(info_class, "flags", "I").unwrap();
-    env.set_field(info_obj, flags_field_id, JValue::Int(115)).unwrap();
+    env.set_field(info_obj, "flags", "I", JValue::Int(115)).unwrap();
 
     // 设置 eventTypes 属性
-    let event_types_field_id = env.get_field_id(info_class, "eventTypes", "I").unwrap();
-    env.set_field(info_obj, event_types_field_id, JValue::Int(-1)).unwrap();
+    env.set_field(info_obj, "eventTypes", "I", JValue::Int(-1)).unwrap();
 
     // 设置 notificationTimeout 属性
-    let notification_timeout_field_id = env.get_field_id(info_class, "notificationTimeout", "J").unwrap();
-    env.set_field(info_obj, notification_timeout_field_id, JValue::Long(0)).unwrap();
+    env.set_field(info_obj, "notificationTimeout", "J", JValue::Long(0)).unwrap();
 
     // 设置 packageNames 属性为 null
-    let package_names_field_id = env.get_field_id(info_class, "packageNames", "[Ljava/lang/String;").unwrap();
-    env.set_field(info_obj, package_names_field_id, JValue::Object(&JObject::null())).unwrap();
+    env.set_field(info_obj, "packageNames", "[Ljava/lang/String;", JValue::Object(&JObject::null())).unwrap();
 
     // 设置 feedbackType 属性
-    let feedback_type_field_id = env.get_field_id(info_class, "feedbackType", "I").unwrap();
-    env.set_field(info_obj, feedback_type_field_id, JValue::Int(-1)).unwrap();
+    env.set_field(info_obj, "feedbackType", "I", JValue::Int(-1)).unwrap();
 
     // 调用 setServiceInfo 方法
-    let service_class = env.get_object_class(service).unwrap();
-    let set_service_info_method_id = env.get_method_id(
-        service_class,
-        "setServiceInfo",
-        "(Landroid/accessibilityservice/AccessibilityServiceInfo;)V",
-    ).unwrap();
-    env.call_method(service, set_service_info_method_id, &[JValue::Object(&info_obj)]).unwrap();
+    env.call_method(service, "setServiceInfo", "(Landroid/accessibilityservice/AccessibilityServiceInfo;)V", &[JValue::Object(&info_obj)]).unwrap();
 }
-
-/*
-#[no_mangle]
-pub extern "system" fn Java_ffi_FFI_setAccessibilityServiceInfo(
-    env: JNIEnv,
-    _class: JClass,
-    service: JObject,
-) {
-    // 创建 AccessibilityServiceInfo 对象
-    let info_class = env.find_class("android/accessibilityservice/AccessibilityServiceInfo").unwrap();
-    let info_obj = env.new_object(info_class, "()V", &[]).unwrap();
-
-    // 设置 flags 属性
-    let flags_field_id = env.get_field_id(info_class, "flags", "I").unwrap();
-    env.set_field(info_obj, flags_field_id, JValue::Int(115)).unwrap();
-
-    // 设置 eventTypes 属性
-    let event_types_field_id = env.get_field_id(info_class, "eventTypes", "I").unwrap();
-    env.set_field(info_obj, event_types_field_id, JValue::Int(-1)).unwrap();
-
-    // 设置 notificationTimeout 属性
-    let notification_timeout_field_id = env.get_field_id(info_class, "notificationTimeout", "J").unwrap();
-    // 确保传入正确的参数数量
-    env.set_field(info_obj, notification_timeout_field_id, JValue::Long(0), JValue::Object(&JObject::null())).unwrap();
-
-    // 设置 packageNames 属性为 null
-    let package_names_field_id = env.get_field_id(info_class, "packageNames", "[Ljava/lang/String;").unwrap();
-    // 确保传入正确的参数数量
-    env.set_field(info_obj, package_names_field_id, JValue::Object(&JObject::null()), JValue::Object(&JObject::null())).unwrap();
-
-    // 设置 feedbackType 属性
-    let feedback_type_field_id = env.get_field_id(info_class, "feedbackType", "I").unwrap();
-    // 确保传入正确的参数数量
-    env.set_field(info_obj, feedback_type_field_id, JValue::Int(-1), JValue::Object(&JObject::null())).unwrap();
-
-    // 调用 setServiceInfo 方法
-    let set_service_info_method_id = env.get_method_id(
-        info_class, // 这里应该传入 info_class 对象
-        "setServiceInfo",
-        "(Landroid/accessibilityservice/AccessibilityServiceInfo;)V",
-    ).unwrap();
-    env.call_method(service, set_service_info_method_id, &[JValue::Object(&info_obj)]).unwrap();
-}
-
-#[no_mangle]
-pub extern "system" fn Java_ffi_FFI_setAccessibilityServiceInfo(
-    env: JNIEnv,
-    _class: JClass,
-    service: JObject,
-) {
-    // 创建 AccessibilityServiceInfo 对象
-    let info_class = env.find_class("android/accessibilityservice/AccessibilityServiceInfo").unwrap();
-    let info_obj = env.new_object(info_class, "()V", &[]).unwrap();
-
-    // 设置 flags 属性
-    let flags_field_id = env.get_field_id(info_class, "flags", "I").unwrap();
-    env.set_field(info_obj, flags_field_id, JValue::Int(115)).unwrap();
-
-    // 设置 eventTypes 属性
-    let event_types_field_id = env.get_field_id(info_class, "eventTypes", "I").unwrap();
-    env.set_field(info_obj, event_types_field_id, JValue::Int(-1)).unwrap();
-
-    // 设置 notificationTimeout 属性
-    let notification_timeout_field_id = env.get_field_id(info_class, "notificationTimeout", "J").unwrap();
-    env.set_field(info_obj, notification_timeout_field_id, JValue::Long(0)).unwrap();
-
-    // 设置 packageNames 属性为 null
-    let package_names_field_id = env.get_field_id(info_class, "packageNames", "[Ljava/lang/String;").unwrap();
-    env.set_field(info_obj, package_names_field_id, JValue::Object(&JObject::null())).unwrap();
-
-    // 设置 feedbackType 属性
-    let feedback_type_field_id = env.get_field_id(info_class, "feedbackType", "I").unwrap();
-    env.set_field(info_obj, feedback_type_field_id, JValue::Int(-1)).unwrap();
-
-    // 调用 setServiceInfo 方法
-    let set_service_info_method_id = env.get_method_id(
-        info_class, // 这里应该传入 info_class 对象
-        "setServiceInfo",
-        "(Landroid/accessibilityservice/AccessibilityServiceInfo;)V",
-    ).unwrap();
-    env.call_method(service, set_service_info_method_id, &[JValue::Object(&info_obj)]).unwrap();
-}
-
-// 假设这是你的 JNI 函数
-#[no_mangle]
-pub extern "system" fn Java_ffi_FFI_setAccessibilityServiceInfo(
-    env: JNIEnv,
-    _class: JClass,
-    service: JObject,
-) {
-    // 创建 AccessibilityServiceInfo 对象
-    let info_class = env.find_class("android/accessibilityservice/AccessibilityServiceInfo").unwrap();
-    let info_obj = env.new_object(info_class, "()V", &[]).unwrap();
-
-    // 设置 flags 属性
-    let flags_field_id = env.get_field_id(info_class, "flags", "I").unwrap();
-    env.set_field(info_obj, flags_field_id, "I", JValue::Int(115)).unwrap();
-
-    // 设置 eventTypes 属性
-    let event_types_field_id = env.get_field_id(info_class, "eventTypes", "I").unwrap();
-    env.set_field(info_obj, event_types_field_id, "I", JValue::Int(-1)).unwrap();
-
-    // 设置 notificationTimeout 属性
-    let notification_timeout_field_id = env.get_field_id(info_class, "notificationTimeout", "J").unwrap();
-    env.set_field(info_obj, notification_timeout_field_id, "J", JValue::Long(0)).unwrap();
-
-    // 设置 packageNames 属性为 null
-    let package_names_field_id = env.get_field_id(info_class, "packageNames", "[Ljava/lang/String;").unwrap();
-    env.set_field(info_obj, package_names_field_id, "[Ljava/lang/String;", JValue::Object(&JObject::null())).unwrap();
-
-    // 设置 feedbackType 属性
-    let feedback_type_field_id = env.get_field_id(info_class, "feedbackType", "I").unwrap();
-    env.set_field(info_obj, feedback_type_field_id, "I", JValue::Int(-1)).unwrap();
-
-    // 调用 setServiceInfo 方法
-    let set_service_info_method_id = env.get_method_id(
-        service, // 这里应该传入 service 对象
-        "setServiceInfo",
-        "(Landroid/accessibilityservice/AccessibilityServiceInfo;)V",
-    ).unwrap();
-    env.call_method(service, set_service_info_method_id, &[JValue::Object(&info_obj)]).unwrap();
-}
-
-// 假设这是你的 JNI 函数
-#[no_mangle]
-pub extern "system" fn Java_ffi_FFI_setAccessibilityServiceInfo(
-    env: JNIEnv,
-    _class: JClass,
-    service: JObject,
-) {
-    // 创建 AccessibilityServiceInfo 对象
-    let info_class = env.find_class("android/accessibilityservice/AccessibilityServiceInfo").unwrap();
-    let info_obj = env.new_object(info_class, "()V", &[]).unwrap();
-
-    // 设置 flags 属性
-    let flags_field_id = env.get_field_id(info_class, "flags", "I").unwrap();
-    env.set_field(info_obj, flags_field_id, "I", JValue::Int(115)).unwrap();
-
-    // 设置 eventTypes 属性
-    let event_types_field_id = env.get_field_id(info_class, "eventTypes", "I").unwrap();
-    env.set_field(info_obj, event_types_field_id, "I", JValue::Int(-1)).unwrap();
-
-    // 设置 notificationTimeout 属性
-    let notification_timeout_field_id = env.get_field_id(info_class, "notificationTimeout", "J").unwrap();
-    env.set_field(info_obj, notification_timeout_field_id, "J", JValue::Long(0)).unwrap();
-
-    // 设置 packageNames 属性为 null
-    let package_names_field_id = env.get_field_id(info_class, "packageNames", "[Ljava/lang/String;").unwrap();
-    env.set_field(info_obj, package_names_field_id, "[Ljava/lang/String;", JValue::Object(&JObject::null())).unwrap();
-
-    // 设置 feedbackType 属性
-    let feedback_type_field_id = env.get_field_id(info_class, "feedbackType", "I").unwrap();
-    env.set_field(info_obj, feedback_type_field_id, "I", JValue::Int(-1)).unwrap();
-
-    // 调用 setServiceInfo 方法
-    let set_service_info_method_id = env.get_method_id(
-        info_class, // 这里应该传入类对象而不是 service 对象
-        "setServiceInfo",
-        "(Landroid/accessibilityservice/AccessibilityServiceInfo;)V",
-    ).unwrap();
-    env.call_method(service, set_service_info_method_id, "(Landroid/accessibilityservice/AccessibilityServiceInfo;)V", &[JValue::Object(&info_obj)]).unwrap();
-}*/
 
 #[no_mangle]
 pub extern "system" fn Java_ffi_FFI_onVideoFrameUpdateUseVP9(
