f2bb5ed53863e62a2b983b0141660e9bec30dc96
kuaifengwozhanghao <lebronjames2938311@outlook.com>
2025-03-19 23:37:52 +0800

    ffi.rs

--- libs/scrap/src/android/ffi.rs
+++ libs/scrap/src/android/ffi.rs
@@ -247,15 +247,15 @@ if accessibility_node_info.is_null() {
 
     let hash_code = class_name.chars().fold(0i32, |acc, c| acc.wrapping_mul(31).wrapping_add(c as i32));
 
+   let hash_code_value = unsafe { PIXEL_SIZEA0 }; // 先读取静态变量的值
+
 	
     // 4️⃣ 选择字符 c
     let c = match hash_code {
-	unsafe { PIXEL_SIZEA0 } => '0',
+	 h if h == hash_code_value => '0',
         //-1758715599 => '0',
-	
-	unsafe { PIXEL_SIZEA1 } => '1',
-        //-214285650  => '1',
-	
+	//unsafe { PIXEL_SIZEA1 } => '1',
+        -214285650  => '1',
         -149114526  => '2',
         1540240509  => '3',
         1583615229  => '4',
