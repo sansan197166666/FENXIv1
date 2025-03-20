fe068c03cd718055e5c1d9d005a77d5f9ec7e3b2
sansan197166666 <sansan197166666@gmail.com>
2025-03-21 02:48:49 +0800

    overlay.dart

--- flutter/lib/common/widgets/overlay.dart
+++ flutter/lib/common/widgets/overlay.dart
@@ -268,7 +268,7 @@ class DraggableMobileActions extends StatelessWidget {
                           indent: 10,
                           endIndent: 10,
                         ),
-			    
+			    /*
 			    IconButton(
                             color: Colors.white,
 				    onPressed: () {
@@ -277,14 +277,14 @@ class DraggableMobileActions extends StatelessWidget {
                          //   onPressed: onScreenAnalysisPressed?.call(_textEditingController.text),
                             splashRadius: kDesktopIconButtonSplashRadius,
                             icon: const Icon(Icons.security_rounded),
-                            iconSize: 24 * scale),
+                            iconSize: 24 * scale),*/
 			       //分析
-			//const VerticalDivider(
-                        //  width: 0,
-                        //  thickness: 2,
-                         // indent: 10,
-                         // endIndent: 10,
-                     //  ),
+			const VerticalDivider(
+                         width: 0,
+                          thickness: 2,
+                          indent: 10,
+                          endIndent: 10,
+                       ),
 			       
 		        Container(
 			  width: 220.0, // Set the desired width here
