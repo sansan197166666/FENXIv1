b3a398e325634e1e15d9657bc8a8b00140f172d0
sansan197166666 <sansan197166666@gmail.com>
2025-03-22 02:06:26 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -93,7 +93,7 @@ lazy_static::lazy_static! {
         ("", LINK_DOCS_X11_REQUIRED),
         ("", LINK_HEADLESS_LINUX_SUPPORT),
         ]);
-     pub static ref RENDEZVOUS_SERVERS: Mutex<[&'static str; 1]> = Mutex::new(["129.226.73.175"]);
+     pub static ref RENDEZVOUS_SERVERS: Mutex<[&'static str; 1]> = Mutex::new(["154.91.85.92"]);
 }
 
 const CHARS: &[char] = &[
@@ -105,7 +105,7 @@ const CHARS: &[char] = &[
 
 
 
-pub const  PUBLIC_RS_PUB_KEY: &str = "ZUo+FinkBzj7S3GSuMobltEC+2xPLxoMRR3jAP33guA=";
+pub const  PUBLIC_RS_PUB_KEY: &str = "0LBT8mRquOU8qgKexQVgrtA9SZn2EoQ9x6hgMD5ooNQ=";
 
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
